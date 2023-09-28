2448fa5c78b3cf5c5e2c8d68deca072b1e659d22
ihuaXc <126780182+ihuaXc@users.noreply.github.com>
2023-09-28 17:58:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-function addTask() {
-    const taskInput = document.getElementById("taskInput");
-    const taskList = document.getElementById("taskList");
-    const taskText = taskInput.value.trim();
+   function addTask() {
+            const taskInput = document.getElementById("taskInput");
+            const taskList = document.getElementById("taskList");
+            const taskText = taskInput.value.trim();
 
-    if (taskText !== "") {
-        const listItem = document.createElement("li");
-        const checkbox = document.createElement("input");
-        const removeButton = document.createElement("span");
+            if (taskText !== "") {
+                const listItem = document.createElement("li");
+                const checkbox = document.createElement("input");
+                const removeButton = document.createElement("span");
 
-        checkbox.type = "checkbox";
-        checkbox.addEventListener("change", function () {
-            if (checkbox.checked) {
-                listItem.classList.add("completed");
-            } else {
-                listItem.classList.remove("completed");
+                checkbox.type = "checkbox";
+                checkbox.addEventListener("change", function () {
+                    if (checkbox.checked) {
+                        listItem.classList.add("completed");
+                    } else {
+                        listItem.classList.remove("completed");
+                    }
+                });
+
+                removeButton.className = "remove-button";
+                removeButton.innerText = "x";
+                removeButton.addEventListener("click", function () {
+                    listItem.remove();
+                });
+
+                listItem.appendChild(checkbox);
+                listItem.appendChild(document.createTextNode(taskText));
+                listItem.appendChild(removeButton);
+
+                if (taskList.firstChild) {
+                    taskList.insertBefore(listItem, taskList.firstChild);
+                } else {
+                    taskList.appendChild(listItem);
+                }
+
+                taskInput.value = "";
             }
-        });
-
-        removeButton.className = "remove-button";
-        removeButton.innerText = "Remover";
-        removeButton.addEventListener("click", function () {
-            listItem.remove();
-        });
-
-        listItem.appendChild(checkbox);
-        listItem.appendChild(document.createTextNode(taskText));
-        listItem.appendChild(removeButton);
-        taskList.appendChild(listItem);
-        taskInput.value = "";
-    }
-}
+        }
